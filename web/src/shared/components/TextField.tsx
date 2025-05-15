106ea8d8716b27d822f797f8677bd9a4030fee77
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-05-15 00:03:32 -0300

    feat(front-end): adicionar componentes TextArea e TextField customizados com suporte a tema
    - Criação de TextArea e TextField com estilização via Tailwind CSS
    - Integração com tema escuro e responsividade
    - Componentes reutilizáveis com foco em acessibilidade e consistência visual

---
+++ web/src/shared/components/TextField.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { cn } from '../../utils/cn';
+
+interface InputProps
+	extends React.DetailedHTMLProps<
+		React.InputHTMLAttributes<HTMLInputElement>,
+		HTMLInputElement
+	> {
+	errorMessage?: string;
+}
+
+export const TextField = React.forwardRef<HTMLInputElement, InputProps>(
+	({ errorMessage, className, ...props }, ref) => (
+		<div className="relative flex w-full flex-col gap-2">
+			<input
+				{...props}
+				ref={ref}
+				className={cn(
+					'w-full rounded bg-transparent px-4 py-2 text-sm font-medium text-black outline-none ring-1 transition-colors duration-300 placeholder:text-gray-400 ring-transparent dark:text-white dark:placeholder:text-gray-500',
+					{
+						'ring-red': errorMessage,
+						'ring-gray-300 dark:ring-linesDark dark:focus:ring-purple dark:hover:ring-purple focus:ring-purple hover:ring-purple':
+							!errorMessage
+					},
+					className
+				)}
+			/>
+			{errorMessage && (
+				<p className="text-red text-xs font-medium">{errorMessage}</p>
+			)}
+		</div>
+	)
+);
+
+TextField.displayName = 'TextField';
